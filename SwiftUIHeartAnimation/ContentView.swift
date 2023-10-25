317a5e11c74e82caa55cfc9056b36c094fe4bc47
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2023-10-25 14:34:12 +0200

    Initial Commit

---
+++ SwiftUIHeartAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIHeartAnimation
+//
+//  Created by Kaori Persson on 2023-10-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIHeartAnimation/SwiftUIHeartAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHeartAnimationApp.swift
+//  SwiftUIHeartAnimation
+//
+//  Created by Kaori Persson on 2023-10-25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHeartAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
